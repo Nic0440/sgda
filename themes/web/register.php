bd55914198f8ff7145849e11e403ea099895c342
Nic0440 <nicolaslemes.ch585@academico.ifsul.edu.br>
2023-06-21 16:24:04 -0300

    register-script

--- themes/web/register.php
+++ themes/web/register.php
@@ -11,3 +11,23 @@
       <input class="input--register" type="password" name="confirm" placeholder="Confirmar senha" />
    <button type="submit">Enviar</button>
  </form>
+
+ <script type="text/javascript" async>
+    const form = document.querySelector(".php-email-form");
+
+    const headers = {
+        email: "fabiosantos@ifsul.edu.br",
+        password: "12345678"
+    };
+
+    form.addEventListener("submit", async (e) => {
+        e.preventDefault(); 
+        const data = await fetch(`<?= url("api/user");?>`,{
+            method: "POST",
+            body: new FormData(form),
+            headers: headers
+        });
+        const user = await data.json();
+        console.log(user);
+    });
+</script>
