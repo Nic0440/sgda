92742eef97409af71e84d64332809024da664854
Nic0440 <nicolaslemes.ch585@academico.ifsul.edu.br>
2023-06-21 16:27:00 -0300

    category

---
+++ source/Models/CategoryClass.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Source\Models;
+
+use Source\Core\Connect;
+
+class Category
+{
+
+    public function selectAll()
+    {
+        $stm = Connect::getInstance()->query("SELECT * FROM categories");
+        return $stm->fetchAll();
+    }
+
+}
